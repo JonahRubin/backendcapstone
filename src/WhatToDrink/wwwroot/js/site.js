554fc1bf058137da22e1c8e940c3df9483678c3d
David Yunker <David Yunker>
2016-12-11 10:19:21 -0600

    added images, breweries to other pages. Continued working with site.js to chain dropdowns

--- src/WhatToDrink/wwwroot/js/site.js
+++ src/WhatToDrink/wwwroot/js/site.js
@@ -2,23 +2,34 @@
 $(document).ready(function () {
     console.log('ready')
 
- /*   $("#Beer_SeasonId").on("change", function (e) {
-        $.ajax({
-            url: `/Beer/GetFeelings/`,
-            method: "POST",
-            dataType: "json",
-            contentType: 'application/json; charset=utf-8'
-        }).done((feelings) => {
-            $("#Beer_FeelingId").html("");
-            $("#Beer_FeelingId").append("<option value=null> How are you feeling? </option>");
-            feelings.forEach((option) => {
-                console.log("these are the options", option);
-                $("#Beer_FeelingId").append(`<option value="${option.FeelingId}">${option.description}</option>`)
-            });
-        });
-    });*/
+
 
     $("#Beer_SeasonId").on("change", function (e) {
+        var x = $(this).val();
+        console.log(x, "this is x");
+        $("#Beer_FeelingId").on("change", function (e) {
+            var y = $(this).val();
+            console.log(y, "this is y");
+            $("#Beer_DayId").on("change", function (e) {
+                var z = $(this).val();
+                console.log(z, "this is z");
+                $.ajax({
+                    url: `/Beer/GetBeersByAll/${x}/${y}/${z}`,
+                    method: "POST",
+                    dataType: "json",
+                    contentType: "application/json; charset=utf-8"
+                }).done((beersByAll) => {
+                     $("#AllBeersGoHere").html("");
+                     beersByAll.forEach((beer) => {
+                         console.log("these are the beers", beer);
+                         $("#AllBeersGoHere").append(`<h3>${beer.name}</h3>`)
+                     });
+                 });
+            });
+        });
+    });
+
+    $("#Beer_Season").on("change", function (e) {
         console.log('SeasonId clicked')
         $.ajax({
             url: `/Beer/GetBeersBySeason/${$(this).val()}`,
@@ -29,12 +40,13 @@ $(document).ready(function () {
             $("#BeersGoHere").html("");
             beersBySeason.forEach((beer) => {
                 console.log("these are the beers", beer);
-                $("#BeersGoHere").append(`<h3>${beer.name}</h3>`)
+                $("#BeersGoHere").append(`<div><img src=${beer.imgUrl}><h3>${beer.name}</h3><p>${beer.brewery}</p></div>`)
             });
         });
     });
 
-    $("#Beer_FeelingId").on("change", function (e) {
+
+    $("#Beer_Feeling").on("change", function (e) {
         console.log('FeelingId clicked')
         $.ajax({
             url: `/Beer/GetBeersByFeelings/${$(this).val()}`,
@@ -45,13 +57,13 @@ $(document).ready(function () {
             $("#BeersByFeelingGoHere").html("");
             beersByFeeling.forEach((beer) => {
                 console.log("these are the beers", beer);
-                $("#BeersByFeelingGoHere").append(`<h3>${beer.name}</h3>`)
+                $("#BeersByFeelingGoHere").append(`<div><img src=${beer.imgUrl}><h3>${beer.name}</h3><p>${beer.brewery}</p></div>`)
             });
         });
     });
 
 
-    $("#Beer_DayId").on("change", function (e) {
+    $("#Beer_Day").on("change", function (e) {
         console.log('DayId clicked')
         $.ajax({
             url: `/Beer/GetBeersByDay/${$(this).val()}`,
@@ -62,7 +74,7 @@ $(document).ready(function () {
             $("#BeersByDayGoHere").html("");
             beersByDay.forEach((beer) => {
                 console.log("these are the beers", beer);
-                $("#BeersByDayGoHere").append(`<h3>${beer.name}</h3>`)
+                $("#BeersByDayGoHere").append(`<div><img src=${beer.imgUrl}><h3>${beer.name}</h3><p>${beer.brewery}</p></div>`)
             });
         });
     });
